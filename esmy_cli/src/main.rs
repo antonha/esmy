013920faeb62f872ec02d9eefc44c8ae41d64fbb
Anton Hagerstrand <anton.hagerstrand@gmail.com>
2018-04-20 08:12:34 +0000

    Created subcomands for index and searching in the cli.

--- esmy_cli/src/main.rs
+++ esmy_cli/src/main.rs
@@ -1,80 +1,123 @@
+extern crate clap;
 extern crate esmy;
+extern crate serde;
+extern crate serde_json;
 extern crate time;
 
-use esmy::analyzis::WhiteSpaceAnalyzer;
+use clap::{App, Arg, SubCommand};
+use esmy::analyzis::UAX29Analyzer;
 use esmy::index_manager::IndexManager;
 use esmy::search;
-use esmy::seg::{self, FullDoc, StringIndex};
-use std::collections::HashMap;
-use std::env;
-use std::io::BufRead;
+use esmy::seg::{self, Doc, FullDoc, StringIndex};
 use std::ops::Sub;
-use std::path::Path;
+use std::path::PathBuf;
 
 fn main() {
-    let index_path = env::current_dir()
-        .unwrap()
-        .join(&Path::new("tmp/tests/index"));
-    println!("{:?}", index_path);
-    if !index_path.exists() {
-        std::fs::create_dir_all(&index_path).unwrap()
-    } else {
-        std::fs::remove_dir_all(&index_path).unwrap()
-    }
-    let start_index = time::now();
-    let reader = std::io::BufReader::new(
-        std::fs::File::open("/home/anton/dev/off/wiki_dump/bodies.txt").unwrap(),
-    );
-    let features: Vec<Box<seg::Feature>> = vec![
-        Box::new(StringIndex::new("text", Box::from(WhiteSpaceAnalyzer {}))),
-        Box::new(FullDoc::new()),
-    ];
-    let index = seg::Index::new(seg::SegmentSchema { features }, index_path);
-    let mut index_manager = IndexManager::new(index);
-    let mut i = 0i64;
-    for line in reader.lines() {
-        let body = line.unwrap();
-        let mut doc = HashMap::new();
-        doc.insert("text".to_owned(), seg::FieldValue::String(body));
-        index_manager.add_doc(doc);
-        i += 1;
-        if i % 50000 == 0 {
-            let used = time::now().sub(start_index).num_milliseconds();
-            println!(
-                "Written: {} took: {}, dps: {}",
-                i,
-                used,
-                (i) / (1 + used / 1000)
+    let matches =
+        App::new("esmy")
+            .version("0.1.0")
+            .author("Anton Hägerstrand <anton.hagerstrand@gmail.com>")
+            .about("CLI interface for Esmy")
+            .subcommand(
+                SubCommand::with_name("index")
+                    .about("Indexes documents.")
+                    .arg(
+                        Arg::with_name("path").short("p").default_value(".").help(
+                            "The path to index at. Defaults to the current working directory.",
+                        ),
+                    )
+                    .arg(
+                        Arg::with_name("clear")
+                            .short("c")
+                            .help("If the index path should be cleared before indexing.."),
+                    )
+                    .arg(
+                        Arg::with_name("v")
+                            .short("v")
+                            .multiple(true)
+                            .help("Sets the level of verbosity"),
+                    ),
+            )
+            .subcommand(
+                SubCommand::with_name("search")
+                    .about("Searches for documents.")
+                    .arg(
+                        Arg::with_name("path").short("p").default_value(".").help(
+                            "The path to index at. Defaults to the current working directory.",
+                        ),
+                    )
+                    .arg(
+                        Arg::with_name("QUERY")
+                            .required(true)
+                            .index(1)
+                            .help("If the index path should be cleared before indexing.."),
+                    ),
+            )
+            .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("index") {
+        let index_path = PathBuf::from(matches.value_of("path").unwrap());
+        let verbose = matches.occurrences_of("v") > 0;
+        let clear = matches.is_present("c");
+
+        if verbose {
+            eprintln!("Will index into '{:?}'", &index_path);
+        }
+        if !index_path.exists() {
+            std::fs::create_dir_all(&index_path).unwrap()
+        } else if clear {
+            std::fs::remove_dir_all(&index_path).unwrap()
+        }
+        let features: Vec<Box<seg::Feature>> = vec![
+            Box::new(StringIndex::new("body", Box::from(UAX29Analyzer {}))),
+            Box::new(FullDoc::new()),
+        ];
+        let index = seg::Index::new(seg::SegmentSchema { features }, index_path);
+        let mut index_manager = IndexManager::new(index);
+        let start_index = time::now();
+        let stream = serde_json::Deserializer::from_reader(std::io::BufReader::new(
+            std::io::stdin(),
+        )).into_iter::<Doc>();
+        let mut i = 0i64;
+        for doc in stream {
+            index_manager.add_doc(doc.unwrap());
+            i += 1;
+            if verbose && i % 50000 == 0 {
+                let used = time::now().sub(start_index).num_milliseconds();
+                eprintln!(
+                    "Written: {} took: {}, dps: {}",
+                    i,
+                    used,
+                    (i) / (1 + used / 1000)
+                );
+            }
+        }
+        index_manager.commit().unwrap();
+        if verbose {
+            eprintln!(
+                "Indexing took: {0}",
+                time::now().sub(start_index).num_milliseconds()
             );
         }
     }
-    println!(
-        "Indexing took: {0}",
-        time::now().sub(start_index).num_milliseconds()
-    );
+    if let Some(matches) = matches.subcommand_matches("search") {
+        let index_path = PathBuf::from(matches.value_of("path").unwrap());
+        let query_string = matches.value_of("QUERY").unwrap();
 
-    /*
-    println!("Startnig merging");
-    let start_merge = time::now();
-    index.merge(&index.list_segments()).unwrap();
-    println!(
-        "Merging took: {0}",
-        time::now().sub(start_merge).num_milliseconds()
-    );*/
-
-    let index_reader = index_manager.open_reader();
-    let f2 = std::fs::File::open("/usr/share/dict/american-english").unwrap();
-    let file2 = std::io::BufReader::new(&f2);
-    let lines = file2.lines().take(100000).map(|l| l.unwrap());
-    let analyzer = esmy::analyzis::UAX29Analyzer {};
-    for line in lines.take(1000) {
+        let features: Vec<Box<seg::Feature>> = vec![
+            Box::new(StringIndex::new("body", Box::from(UAX29Analyzer {}))),
+            Box::new(FullDoc::new()),
+        ];
+        let index = seg::Index::new(seg::SegmentSchema { features }, index_path);
+        let index_reader = index.open_reader();
+        let analyzer = esmy::analyzis::UAX29Analyzer {};
         let start_search = time::now();
         let mut collector = search::AllDocsCollector::new();
-        let query = search::TextQuery::new("text", &line, &analyzer);
+        let query = search::TextQuery::new("body", &query_string, &analyzer);
         search::search(&index_reader, &query, &mut collector).unwrap();
         println!(
             "Word '{}' had {} matches, took {} ms",
-            line,
+            query_string,
             collector.docs().len(),
             time::now().sub(start_search).num_milliseconds() as f32
         );
